dd33018da2b06900b7bab1eccc89ce94d91c6518
Meliah MacKay <xmeilahmackay@gmail.com>
2020-02-09 15:53:24 -0800

    add first form question.

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+  $(".art").click(function() {
+    $("#doodle").toggle();
+    $("#doodle2").toggle();
+    $("#doodle3").toggle();
+    $("#doodle4").toggle();
+  });
+
+});
